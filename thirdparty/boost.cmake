9b66be0d42dc22f8a8e3c6b14def0bbadf52aab3
Mikhail Smirnov <cpp4ever@gmail.com>
2026-08-30 17:18:41 +0300

    Bump `boost` from 1.90.0 to 1.92.0

--- thirdparty/boost.cmake
+++ thirdparty/boost.cmake
@@ -31,8 +31,8 @@ FetchContent_Declare(
    EXCLUDE_FROM_ALL
    SYSTEM
    # Download Step Options
-   URL https://github.com/boostorg/boost/releases/download/boost-1.90.0/boost-1.90.0-cmake.tar.xz
-   URL_HASH SHA256=aca59f889f0f32028ad88ba6764582b63c916ce5f77b31289ad19421a96c555f
+   URL https://github.com/boostorg/boost/releases/download/boost-1.92.0/boost-1.92.0-cmake.tar.xz
+   URL_HASH SHA256=9bed76128d4e46755dbe818487788c6fceb6f72b378f4daa49b7e1e600d9088d
    DOWNLOAD_EXTRACT_TIMESTAMP ON
 )
 FetchContent_MakeAvailable(boost)
